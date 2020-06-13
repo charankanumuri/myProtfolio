bb00b0fdf3d616c074044fd8d0b59c4a67c6e6d3
charankanumuri <charan.kanumuri@gmail.com>
2020-06-13 16:00:51 +0530

    Added SNS service in upload portfolio lambda function.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,20 +6,25 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
 
-    portfolio_bucket = s3.Bucket('portfolio.4myiot.net')
-    build_bucket = s3.Bucket('portfoliobuild.4myiot.net')
+    topic = sns.Topic('arn:aws:sns:ap-south-1:705123275076:DeployPortfolioTopic')
 
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        portfolio_bucket = s3.Bucket('portfolio.4myiot.net')
+        build_bucket = s3.Bucket('portfoliobuild.4myiot.net')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print("Job Done!!!")
-    return {
-            'statusCode': 200,
-            'body': json.dumps('Hello from Lambda!')
-        }
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print("Job Done!!!")
+        topic.publish(Subject="Portfolio Deployed!!!", Message="Your Portfolio deployed successfully.")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed...", Message="Your Portfolio deployment was not successful.")
+        raise
+    return {'body': json.dumps('Hello from Lambda!')}
